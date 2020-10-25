94d50cb6a790552e80df4abd709ab744e28f6ed2
trex22 <contact@jasonchalom.com>
2020-10-25 23:51:39 +0200

    Fix accidental missing requires :cop:

--- lib/pushbullet.rb
+++ lib/pushbullet.rb
@@ -5,6 +5,13 @@ require 'pushbullet/version'
 require 'pushbullet/constants'
 
 # Endpoints
+require 'pushbullet/chats'
+require 'pushbullet/devices'
+require 'pushbullet/permanents'
+require 'pushbullet/pushes'
+require 'pushbullet/subscriptions'
+require 'pushbullet/texts'
+require 'pushbullet/users'
 
 require 'pushbullet/client'
 
